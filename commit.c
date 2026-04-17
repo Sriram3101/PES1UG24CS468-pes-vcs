13b3cadb88b6a89c1bcdf163470b843b90daeda0
Sriram3101 <ksriram3101@gmail.com>
2026-04-17 22:31:29 +0530

    phase 4 - 1. Generate root tree from current index
    Removed comments and documentation about commit structure and provided functions.

--- commit.c
+++ commit.c
@@ -1,18 +1,4 @@
-// commit.c — Commit creation and history traversal
-//
-// Commit object format (stored as text, one field per line):
-//
-//   tree <64-char-hex-hash>
-//   parent <64-char-hex-hash>        ← omitted for the first commit
-//   author <name> <unix-timestamp>
-//   committer <name> <unix-timestamp>
-//
-//   <commit message>
-//
-// Note: there is a blank line between the headers and the message.
-//
-// PROVIDED functions: commit_parse, commit_serialize, commit_walk, head_read, head_update
-// TODO functions:     commit_create
+
 
 #include "commit.h"
 #include "index.h"
@@ -24,6 +10,9 @@
 #include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
+
+
+
 
 // Forward declarations (implemented in object.c)
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out);
